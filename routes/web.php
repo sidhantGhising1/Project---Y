210aec26cad313e95b3b10648b12e7b6e1fb5b09
sidhantGhising1 <sidhantghising95@gmail.com>
2026-01-28 22:44:06 +0545

    First commit

---
+++ routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PostController::class, 'index'])
+    ->middleware('auth')
+    ->name('home');
+
+Route::get('/home', function () {
+    return redirect('/');
+})->name('home');
+
+Auth::routes();
+
+// Post routes
+Route::get('/posts', [PostController::class, 'index'])
+    ->name('posts.index');
+
+Route::get('/posts/create', [PostController::class, 'create'])
+    ->name('posts.create');
+
+Route::post('/posts', [PostController::class, 'store'])
+    ->name('posts.store');
+
+Route::get('/posts/{post}', [PostController::class, 'show'])
+    ->name('posts.show');
+
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])
+    ->name('posts.edit');
+
+Route::patch('/posts/{post}', [PostController::class, 'update'])
+    ->name('posts.update');
+
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])
+    ->name('posts.destroy');
+
+// Profile routes
+Route::get('/profile/{user}', [ProfileController::class, 'index'])
+    ->name('profile.show');
+
+Route::get('/profile/{user}/edit', [ProfileController::class, 'edit'])
+    ->name('profile.edit');
+
+Route::patch('/profile/{user}', [ProfileController::class, 'update'])
+    ->name('profile.update');
+
+// Comment routes
+Route::post('/posts/{post}/comments', [CommentController::class, 'store'])
+    ->name('comments.store');
+
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])
+    ->name('comments.destroy');
+
+// Like routes
+Route::post('/posts/{post}/likes', [LikeController::class, 'store'])
+    ->name('likes.store');
+
+Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])
+    ->name('likes.destroy');
